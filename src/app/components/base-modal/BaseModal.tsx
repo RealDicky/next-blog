3b20392735b1677ed61fb38c8035570764a7812f
zjx <13317176014@qq.com>
2024-07-31 12:07:51 +0800

    feat:cache readed note

--- src/app/components/base-modal/BaseModal.tsx
+++ src/app/components/base-modal/BaseModal.tsx
@@ -10,9 +10,7 @@ const BaseModal: FC<PropsWithChildren<{
       props.visible
         ? (
           <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
-            <div className="w-1/3 bg-white rounded-md p-4">
-              {/* <h1 className="text-xl font-bold mb-4">Modal Title</h1>
-              <p>Modal Content</p> */}
+            <div className="w-1/3 min-w-72 bg-white rounded-md p-4">
               {props.children}
             </div>
           </div>
